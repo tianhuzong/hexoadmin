dc88ced36bc0bad812c72efbe8ee3a9a225dddb7
tianhuzong <tianhuzong@qq.com>
2024-01-13 16:37:44 +0800

    将jiexitext函数修改为CSDN上的，更新了requirements.txt

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from loguru import logger
 import subprocess
 import time
 import os
+import yaml
 app = Flask(__name__)
 
 # function tools 一些函数工具
@@ -21,7 +22,7 @@ def get_middle_text(text, start_marker, end_marker):
     
     middle_text = text[start_index:end_index]
     return middle_text.lstrip()
-def creat_page(path:str,title:str, tags:list, categories:list,date:str = gettime(),posts:bool = True,text_content:str = ""):
+def creat_page(path:str,title:str, tags:list, categories:list,date:str = gettime(),posts :str = "post",text_content:str = ""):
     """
     创建一篇新的文章
     :param path hexo项目的根目录
@@ -29,7 +30,7 @@ def creat_page(path:str,title:str, tags:list, categories:list,date:str = gettime
     :param tags 文章标签的列表
     :param categories 文章分类的列表，列表后面的元素是前面的子分类
     :param date 创建文章的时间
-    :param posts 是否为post，false则为草稿
+    :param posts 文件布局,默认为post.选项如下:post,draft,page。post为发布的文章,draft为草稿,page为页面(例如关于页,友链页) 
     :param text_content 文章正文，默认为空 
     """
     #生成标签的文本
@@ -47,15 +48,11 @@ date: {date}
 {categories_text}
 ---
 """
-    if posts == True:
-        post = "post"
-    else : 
-        post = "draft"
     os.chdir(path=path)
-    subprocess.run(["hexo","new",post,title]) #生成hexo文章文件
+    subprocess.run(["hexo","new",posts,title]) #生成hexo文章文件
     with open(path + r"/source/_" + post + "s/" + title + ".md",encoding="utf8",mode="w") as f :
         f.write(content + text_content)
-def jiexitext(path):
+def jiexitext_myself(path):
     """
     解析一篇文章
     :param path 文章地址
@@ -108,4 +105,50 @@ def jiexitext(path):
         jiexires[index] = get_middle_text(head[x],":","\n")
     jiexires["body"] = body
     return jiexires
-    
+
+'''分割符号匹配检索'''
+def pattern_search(string,pattern):
+    index=0
+    while index<len(string)-len(pattern):
+        index=string.find(pattern,index,len(string))
+        if index==-1:
+            break
+        yield index
+        index+=len(pattern)-1
+
+"""
+版权声明：本文为CSDN博主「xiejava1018」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
+原文链接：https://blog.csdn.net/fullbug/article/details/126007706
+"""
+
+def jiexitext(blog_md_file):
+    #读md文件
+    md_f = open(blog_md_file, "r",encoding='utf-8')
+    md_f_str=md_f.read()
+    #解析两个---之间的内容
+    pattern='---'
+    blog_data={}
+    pattern_list=list(pattern_search(md_f_str, pattern))
+    if len(pattern_list)>=2:
+        blog_info_str=md_f_str[pattern_list[0]+len(pattern):pattern_list[1]]
+        blog_data=yaml.load(blog_info_str,Loader=yaml.SafeLoader)
+        blog_data['content']=md_f_str[pattern_list[1]+len(pattern):]
+    md_f.close()
+    return blog_data
+
+
+def pattern_search(string,pattern):
+    '''分割符号匹配检索'''
+    index=0
+    while index<len(string)-len(pattern):
+        index=string.find(pattern,index,len(string))
+        if index==-1:
+            break
+        yield index
+        index+=len(pattern)-1
+def update_page(path,text_content):
+    """
+    更新文章内容，不需要编写文章头部内容
+    :path 文章目录
+    """
+    pass   
